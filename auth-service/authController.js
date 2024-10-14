51480f65e1fa280ed7b2923a6f2f389d67599669
Ilya <ilieturcan424@gmail.com>
2024-10-14 11:45:13 +0300

    Add project v1.4.0

--- auth-service/authController.js
+++ auth-service/authController.js
@@ -1,60 +1,8 @@
-// const User = require("./models/User");
-// const Role = require("./models/Role");
-// const bcrypt = require("bcryptjs");
-// const jwt = require("jsonwebtoken");
-// const { validationResult } = require("express-validator");
-// const { secret } = require("./config");
-
-// const generateAccessToken = (id, roles) => {
-// 	const payload = {
-// 		id,
-// 		roles
-// 	};
-// 	return jwt.sign(payload, secret, {expiresIn: "24h"} );
-// };
-
-// class authController {
-// 	async registration(req, res) {
-
-// 	}
-
-// 	async login(req, res) {
-// 		try {
-// 			const {username, password} = req.body;
-// 			const user = await User.findOne({username});
-// 			if (!user) {
-// 				return res.status(400).json({message: `Пользователь ${username} не найден`});
-// 			}
-// 			const validPassword = bcrypt.compareSync(password, user.password);
-// 			if (!validPassword) {
-// 				return res.status(400).json({message: "Введен неверный пароль"});
-// 			}
-// 			const token = generateAccessToken(user._id, user.roles);
-// 			return res.json({token});
-// 		} catch (e) {
-// 			console.log(e);
-// 			res.status(400).json({message: "Login error"});
-// 		}
-// 	}
-
-// 	async getUsers(req, res) {
-// 		try {
-// 			const users = await User.find();
-// 			res.json(users);
-// 		} catch (e) {
-			
-// 		}
-// 	}
-// }
-
-// module.exports = new authController();
-
 const User = require("./models/User"); // Модель пользователя из базы данных
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const Role = require("./models/Role");
-const { validationResult } = require("express-validator");
-const { secret } = require("./config");
+
 
 // Логин пользователя
 exports.loginUser = async (req, res) => {
--- auth-service/authRouter.js
+++ auth-service/authRouter.js
@@ -1,24 +1,11 @@
 const Router = require("express");
 const router = new Router();
 const { loginUser, registerUser, getUsers } = require("./authController");
-const { check } = require("express-validator");
 
-router.post("/registration", [
-	check("username", "Имя пользователя не может быть пустым").notEmpty(),
-	check("password", "Пароль должен быть больше 4 и меньше 10 символов").isLength({min:4, max:10})
-], registerUser);
+
 router.post("/login", loginUser);
 router.post("/register", registerUser);
 router.get("/users", getUsers);
 
 module.exports = router;
 
-// const express = require("express");
-// const { loginUser, registerUser, getUsers } = require("./authController");
-// const router = express.Router();
-
-// router.post("/login", loginUser);
-// router.post("/register", registerUser);
-// router.get("/users", getUsers);
-
-// module.exports = router;
